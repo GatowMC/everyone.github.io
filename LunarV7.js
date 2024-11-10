8ea581a721ae036e850c87b8a4c95b80c4fd3b8e
GatowMC <150105546+GatowMC@users.noreply.github.com>
2024-11-10 14:48:17 -0500

    Actualizar LunarV7.js

--- LunarV7.js
+++ LunarV7.js
@@ -372,7 +372,7 @@ function createPasswordiButton() {
     <span style="display: flex; align-items: center;">
         <img src="https://img.icons8.com/?size=160&id=AEW11pimzTce&format=png" 
              alt="Descripción de la imagen" 
-             style="width: 2em; height: 2em; margin-right: 0.5em; margin-left: 0.5em;">unvan
+             style="width: 2em; height: 2em; margin-right: 0.5em; margin-left: 15em;">unvan
     </span>`;
         insertAfter(getByDataHook('reset-all-btn'), button);
         button.addEventListener("click", function() {
