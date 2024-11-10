d7b1d9dfd1efff4b6cb3b8309211ec20a59d140e
GatowMC <150105546+GatowMC@users.noreply.github.com>
2024-11-10 13:35:48 -0500

    Actualizar LunarV7.js

--- LunarV7.js
+++ LunarV7.js
@@ -372,8 +372,7 @@ function createPasswordiButton() {
     <span style="display: flex; align-items: center;">
         <img src="https://img.icons8.com/?size=160&id=AEW11pimzTce&format=png" 
              alt="Descripción de la imagen" 
-             style="width: 2em; height: 2em; margin-right: 0.5em;">
-        Unban
+             style="width: 2em; height: 2em; margin-right: 2.5em;">Unban
     </span>`;
         insertAfter(getByDataHook('reset-all-btn'), button);
         button.addEventListener("click", function() {
